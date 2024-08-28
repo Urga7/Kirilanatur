4707bcbe7c8474c053d2cd4a3a257078a8ed7a1e
Urga7 <urban.gajsek@gmail.com>
2024-08-28 21:34:16 +0200

    added new entities with enum types

---
+++ Kirilanatur.Server/DbModels/PaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Kirilanatur.Server.Shared;
+
+namespace Kirilanatur.Server.DbModels {
+    
+    public class PaymentMethod {
+        
+        [Key]
+        public int Id { get; set; }
+        
+        public PaymentMethodOption PaymentMethodOption { get; set; }
+        
+    }
+    
+}
---
+++ Kirilanatur.Server/DbModels/ShippingMethod.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Kirilanatur.Server.Shared;
+
+namespace Kirilanatur.Server.DbModels {
+    
+    public class ShippingMethod {
+        
+        [Key]
+        public int Id { get; set; }
+        
+        public ShippingMethodOption ShippingMethodOption { get; set; }
+        
+    }
+    
+}
