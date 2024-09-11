cc5c76e28cc4beb4a3a8f95023f6e8b0203c1b81
Urga7 <urban.gajsek@gmail.com>
2024-09-11 19:41:15 +0200

    displaying images on the frontend

--- kirilanatur.client/src/app/models/DatabaseObjects.ts
+++ kirilanatur.client/src/app/models/DatabaseObjects.ts
@@ -6,11 +6,16 @@
   discount: number,
   available: boolean,
   categoryId: number,
+  images: Image[]
 
-/*
-public ProductCategory? Category { get; set; }
+  /*public ProductCategory? Category { get; set; }
+   public List<ProductItem> ProductItems { get; set; } = [];
+   public List<ProductImage> Images { get; set; } = [];*/
 
-public List<ProductItem> ProductItems { get; set; } = [];
-public List<ProductImage> Images { get; set; } = [];*/
+}
 
+export interface Image {
+  id: number,
+  imageUrl: string,
+  imageDescription: string,
 }
