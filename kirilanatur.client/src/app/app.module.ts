15f7214c930ace7f2f016751ca032b4b6ce23843
Urga7 <urban.gajsek@gmail.com>
2024-09-23 07:37:31 +0200

    some frontend registration page progress

--- kirilanatur.client/src/app/app.module.ts
+++ kirilanatur.client/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomeComponent } from './components/home/home.component';
 import { SandalsComponent } from './components/sandals/sandals.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { RegisterComponent } from './components/register/register.component';
+import { RegistrationFormComponent } from './components/forms/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { RegisterComponent } from './components/register/register.component';
     HomeComponent,
     SandalsComponent,
     AdminComponent,
-    RegisterComponent
+    RegisterComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
