a951d21fa79012524aa3865aed79c78c2b199f3f
Urga7 <urban.gajsek@gmail.com>
2024-10-02 11:28:13 +0200

    frontend form submission

--- kirilanatur.client/src/app/components/register/register.component.ts
+++ kirilanatur.client/src/app/components/register/register.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+  import { Component, OnInit } from '@angular/core';
+  import { FormControl, FormGroup } from '@angular/forms';
+  import { RegistrationForm } from "../../models/RegistrationForm.model";
 
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
-})
-export class RegisterComponent {
+  @Component({
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.css'
+  })
+  export class RegisterComponent implements OnInit {
+    registrationForm = new FormGroup({
+      name: new FormControl(''),
+      surname: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+    });
 
-}
+    ngOnInit() {
+
+    }
+
+    onSubmit() {
+      const form: RegistrationForm = {
+        name: this.registrationForm.value.name ?? "",
+        surname: this.registrationForm.value.surname ?? "",
+        email: this.registrationForm.value.email ?? "",
+        password: this.registrationForm.value.password ?? "",
+      };
+
+      console.log(form);
+    }
+  }
