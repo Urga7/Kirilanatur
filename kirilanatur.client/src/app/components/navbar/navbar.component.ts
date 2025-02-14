592ee93a3076142e1de59be1ff608eb130f1db54
Urga7 <urban.gajsek@gmail.com>
2025-02-14 21:56:26 +0100

    added css only hamburger menu code + compartmentalization of html

--- kirilanatur.client/src/app/components/navbar/navbar.component.ts
+++ kirilanatur.client/src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { LanguageSelectionComponent } from "../choose-language/language-selection.component";
 import { TranslateModule } from '@ngx-translate/core';
+import { LanguageSelectionComponent } from "../language-selection/language-selection.component";
+import { SocialMediaLinksComponent } from "../social-media-links/social-media-links.component";
 
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.css',
     standalone: true,
-    imports: [
-      RouterLink,
-      RouterLinkActive,
-      LanguageSelectionComponent,
-      TranslateModule
-    ]
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    LanguageSelectionComponent,
+    TranslateModule,
+    SocialMediaLinksComponent,
+  ]
 })
 export class NavbarComponent {
 
