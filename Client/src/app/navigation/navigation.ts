e73c84e6179430c6c721b7a9f5c1e437685e7a5e
Urga7 <urban.gajsek@gmail.com>
2025-07-13 15:27:08 +0200

    navigation

--- Client/src/app/navigation/navigation.ts
+++ Client/src/app/navigation/navigation.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component, signal} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {SocialMediaLinks} from '../pages/common/social-media-links/social-media-links';
 
 @Component({
   selector: 'app-navigation',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    RouterLink,
+    SocialMediaLinks
   ],
   templateUrl: './navigation.html',
 })
 export class Navigation {
+  protected readonly navigationExpanded = signal(false);
 
+  toggleNav() {
+    const expanded = !this.navigationExpanded();
+    this.navigationExpanded.set(expanded);
+
+    // Lock or unlock body scroll
+    document.body.style.overflow = this.navigationExpanded()
+      ? 'hidden'
+      : 'auto';
+  }
+
+  scrollToBottom() {
+    this.toggleNav();
+    window.scrollTo({top: document.body.scrollHeight});
+  }
 }
--- Client/src/app/pages/home/home.ts
+++ Client/src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {SocialMediaLinks} from '../common/social-media-links/social-media-links';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    SocialMediaLinks
+  ],
   templateUrl: './home.html',
 })
 export class Home {
